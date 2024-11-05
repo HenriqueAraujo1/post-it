604a4a0972d975103f9bd85a61089635d9d3f60d
Henrique Araujo <kaykimotta123@gmail.com>
2024-11-05 20:47:35 -0300

    front-end finaly

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.static('public')); // conceder acesso aos arquivos estáticos na pasta public
 
 app.get('/', (req, res) => { // rota raiz
-  res.send('Hello World!');
+  res.sendFile(__dirname + '/index.html'); // direciona para a página index.html
 });
 
+app.get('/note/:id', (req, res) => {
+    res.sendFile(__dirname + '/public/note.html'); 
+})
+ 
 app.listen(port, (error) => { // inicia o servidor
    if(error) {
     console.log(`não foi possivel iniciar o servidor ${error}`); // caso de erro
